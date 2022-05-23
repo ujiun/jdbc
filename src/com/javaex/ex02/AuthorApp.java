a45a65f07a116dced0f895543db106e7bdf9fc34
ujiun <ujiun@hanmail.net>
2022-05-24 01:35:01 +0900

    AuthorApp

--- src/com/javaex/ex02/AuthorApp.java
+++ src/com/javaex/ex02/AuthorApp.java
@@ -7,14 +7,14 @@ public class AuthorApp {
 	public static void main(String[] args) {
 		
 		AuthorDao authorDao = new AuthorDao();
-		/*
+		
 		authorDao.authorInsert("김문열", "경북영양");
 		authorDao.authorInsert("박경리", "경상남도 통영");
 		authorDao.authorInsert("유시민", "17대 국회의원");
 		authorDao.authorInsert("기안84", "기안동에서 산 84년생");
 		authorDao.authorInsert("강풀", "온라인 만화가 1세대");
 		authorDao.authorInsert("김영하", "알쓸신잡");
-		*/
+		
 		
 		
 		/*
