74a42e9a5014da116c50681d0d976f47f572ea98
ujiun <ujiun@hanmail.net>
2022-05-24 01:35:20 +0900

    BookVo

---
+++ src/com/javaex/ex02/BookVo.java
@@ -0,0 +1,71 @@
+package com.javaex.ex02;
+
+public class BookVo {
+	//필드
+	private int bookId;
+	private String title;
+	private String pubs;
+	private String pubDate;
+	private String authorName;
+	
+	
+	//생성자
+	public BookVo() {
+	}
+
+	public BookVo(int bookId, String title, String pubs, String pubDate,  String authorName) {
+		this.bookId = bookId;
+		this.title = title;
+		this.pubs = pubs;
+		this.pubDate = pubDate;
+		this.authorName = authorName;
+	}
+	
+	//메소드-gs
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPubs() {
+		return pubs;
+	}
+
+	public void setPubs(String pubs) {
+		this.pubs = pubs;
+	}
+
+	public String getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+	//메소드-일반
+	@Override
+	public String toString() {
+		return "BookVo [bookId=" + bookId + ", title=" + title + ", pubs=" + pubs + ", pubDate=" + pubDate
+				+ ", authorName=" + authorName + "]";
+	}
+}
